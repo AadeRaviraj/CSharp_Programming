b19fe61614ea87f40251d099103f5c766f214471
Raviraj <ravirajaade299@gmail.com>
2026-05-05 00:29:55 +0530

    update

---
+++ LB_Q1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class LB_Q1
+    {
+        public void show()
+        {
+
+
+            Console.WriteLine("Hello from class 1");
+        }
+    }
+}
---
+++ main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Main");
+            LB_Q1 obj1 = new LB_Q1();
+
+            obj1.show();
+        }
+    }
+}
