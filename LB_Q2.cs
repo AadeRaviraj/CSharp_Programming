1f2a360ac80c54f55da59bab1757411b9cfa499b
Raviraj <ravirajaade299@gmail.com>
2026-05-05 23:27:31 +0530

    Code Update

---
+++ LB_Q2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class LB_Q2
+    {
+        // Check if a number is palindrome(same forward & backward)
+
+        //Example:
+
+        //121 → true  
+        //123 → false
+
+
+        int result;
+
+        public bool palindrom(int userInput)
+        {
+            int original = userInput;
+            int rem = 0;
+            bool isPalindrom = true;
+
+
+
+
+
+            while (userInput > 0) {
+                rem = userInput % 10;
+                result = result * 10 + rem;
+                userInput = userInput / 10;
+            }
+
+            if(userInput == original)
+            {
+                return isPalindrom = true;
+            }
+            else
+            {
+                return isPalindrom = false;
+
+            }
+           
+        }
+    }
+}
--- main.cs
+++ main.cs
@@ -10,18 +10,32 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Main");
-            LB_Q1 obj1 = new LB_Q1();
+            //LB_Q1 obj1 = new LB_Q1();
 
-            List<int> result = obj1.add();
+            //List<int> result = obj1.add();
 
 
-            //obj1.add();
+            ////obj1.add();
 
-            foreach (int a in result)
+            //foreach (int a in result)
+            //{
+            //    Console.WriteLine("Array Values : " + a);
+
+            //}
+            Console.WriteLine("Enter a Number to check number is palindrom or not : ");
+            int userInput = int.Parse(Console.ReadLine());
+            LB_Q2 obj2 = new LB_Q2();
+            bool  res = obj2.palindrom(userInput);
+            if (res)
             {
-                Console.WriteLine("Array Values : " + a);
+                Console.WriteLine("It is  palindrom number " );
             }
+            else
+            {
+                Console.WriteLine("It is not a Plaindrom number ");
+
+            }
+
         }
     }
 }
