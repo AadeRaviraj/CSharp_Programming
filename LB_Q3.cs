db09a9249f2d90dd8084021c3aa09cd7924708e9
Raviraj <ravirajaade299@gmail.com>
2026-05-07 23:51:47 +0530

    code update

---
+++ LB_Q3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class LB_Q3
+    {
+        //Find Maximum Element in Array
+
+        //Problem:
+        //Return the largest number in array
+        // Example:
+
+        // [3, 5, 1, 9, 2] → 9
+
+
+      
+    }
+}
--- main.cs
+++ main.cs
@@ -16,6 +16,7 @@ namespace Practice
 
 
             ////obj1.add();
+            ///
 
             //foreach (int a in result)
             //{
